aa7cb605ec189be9e2d9f5278783c75bfc67f233
francis <2443573565@qq.com>
2020-01-15 14:32:21 +0800

    feat: lesson 01

---
+++ vue/app/src/components/01/demo01/slotDemo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>一起喊出我们的slogan</h3>
+        <slot name="slogan">
+
+        </slot>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'slot'
+  }
+</script>
+
+<style scoped>
+
+</style>
